a0f134c2371be8890061b13b9418cfa9677c45ab
Igor Vysotskyi <lgorvysocky@gmail.com>
2019-08-11 22:10:04 +0300

    next more...

--- src/main/java/com/ig/servlet/ChatServlet.java
+++ src/main/java/com/ig/servlet/ChatServlet.java
@@ -18,36 +18,27 @@ import java.io.IOException;
 )
 public class ChatServlet extends HttpServlet {
     private static final Logger log = LogManager.getLogger();
+
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException
-    {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String action = request.getParameter("action");
-        if("list".equals(action))
-        {
+        if("list".equals(action)) {
             log.debug("Listing pending support chats.");
             request.setAttribute("sessions", ChatEndpoint.pendingSessions);
-            request.getRequestDispatcher("/WEB-INF/jsp/view/chat/list.jsp")
-                    .forward(request, response);
+            request.getRequestDispatcher("/WEB-INF/jsp/view/chat/list.jsp").forward(request, response);
         }
-        else
-        {
+        else {
             log.info("Rejected chat servlet GET request with invalid action [{}].", action);
-            response.sendRedirect("tickets");
+            response.sendRedirect("courses");
         }
     }
-
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException
-    {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setHeader("Expires", "Thu, 1 Jan 1970 12:00:00 GMT");
         response.setHeader("Cache-Control","max-age=0, must-revalidate, no-cache");
-
         String action = request.getParameter("action");
         String view = null;
-        switch(action)
-        {
+        switch(action) {
             case "new":
                 log.debug("Accepted new chat request.");
                 request.setAttribute("chatSessionId", 0);
@@ -57,20 +48,17 @@ public class ChatServlet extends HttpServlet {
                 String id = request.getParameter("chatSessionId");
                 if(id == null || !NumberUtils.isDigits(id))
                     response.sendRedirect("chat?list");
-                else
-                {
+                else {
                     log.debug("Pending chat request joined.");
                     request.setAttribute("chatSessionId", Long.parseLong(id));
                     view = "chat";
                 }
                 break;
             default:
-                response.sendRedirect("tickets");
+                response.sendRedirect("courses");
                 break;
         }
-
         if(view != null)
-            request.getRequestDispatcher("/WEB-INF/jsp/view/chat/" + view + ".jsp")
-                    .forward(request, response);
+            request.getRequestDispatcher("/WEB-INF/jsp/view/chat/" + view + ".jsp").forward(request, response);
     }
 }
--- src/main/java/com/ig/servlet/filters/AuthenticationFilter.java
+++ src/main/java/com/ig/servlet/filters/AuthenticationFilter.java
@@ -1,5 +1,9 @@
 package com.ig.servlet.filters;
 
+import com.ig.model.UserAccount;
+import com.ig.servlet.AppUtils;
+import com.ig.servlet.CheckROLE_URL;
+import com.ig.servlet.UserRoleRequestWrapper;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -8,7 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-
+import java.util.List;
 
 
 public class AuthenticationFilter implements Filter {
@@ -17,7 +21,19 @@ public class AuthenticationFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         log.info("log:: doFilter()");
-        HttpSession session = ((HttpServletRequest)request).getSession(false);
+        String servletPath = ((HttpServletRequest) request).getServletPath();
+        log.info("log:: servletPath ---> " + servletPath);
+        UserAccount loginedUser = AppUtils.getLoginedUser(((HttpServletRequest) request).getSession());
+        if (servletPath.equals("/login")) {
+            chain.doFilter(request, response);
+            return;
+        }
+        HttpServletRequest wrapRequest = ((HttpServletRequest) request);
+        if (loginedUser != null) {
+            String userName = loginedUser.getUserName();
+            List<String> roles = loginedUser.getRoles();
+            wrapRequest = new UserRoleRequestWrapper(userName, roles, ((HttpServletRequest) request));
+        }
 
 /*
         if(session == null || session.getAttribute("username") == null || session.getAttribute("uid") == null)
@@ -25,11 +41,33 @@ public class AuthenticationFilter implements Filter {
         else
             chain.doFilter(request, response);
 */
+
+        if (CheckROLE_URL.isSecurityPage(((HttpServletRequest) request))) {
+            if (loginedUser == null) {
+                String requestUri = ((HttpServletRequest) request).getRequestURI();
+                int redirectId = AppUtils.storeRedirectAfterLoginUrl(((HttpServletRequest) request).getSession(), requestUri);
+                ((HttpServletResponse) response).sendRedirect(wrapRequest.getContextPath() + "/login?redirectId=" + redirectId);
+                return;
+            }
+            boolean hasPermission = CheckROLE_URL.hasPermission(wrapRequest);
+            if (!hasPermission) {
+                ((HttpServletResponse) response).sendRedirect("course");
+                return;
+            }
+        }
+
+        chain.doFilter(wrapRequest, response);
     }
 
     @Override
-    public void init(FilterConfig config) throws ServletException { }
+    public void init(FilterConfig fConfig) throws ServletException {
+
+    }
+
+    public AuthenticationFilter() {
+    }
 
     @Override
-    public void destroy() { }
+    public void destroy() {
+    }
 }
